383433bd23de252deb5b5810987ea998e6297fc3
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2021-11-11 10:28:35 -0300

    Projeto/Curso finalizado

--- src/br/com/alura/rh/model/Terceirizado.java
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -1,5 +1,30 @@
 package br.com.alura.rh.model;
 
+import java.math.BigDecimal;
+
 public class Terceirizado {
+	
+	private DadosPessoais dadosPessoais;
+	private String nomeEmpresa;
+	
+	public Terceirizado(String nomeEmpresa, String nome, String cpf, Cargo cargo, BigDecimal salario) {
+		this.nomeEmpresa = nomeEmpresa;
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+	}
+		
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
+	}
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
+	}
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+	
+	
 
 }
